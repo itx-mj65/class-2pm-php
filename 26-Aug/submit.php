e0343ce833412ac60bf7e46cde0a02d67c29d8aa
itx-mj65 <jamshedweb65@gmail.com>
2025-08-26 14:47:36 +0500

    26 of august

---
+++ 26-Aug/submit.php
@@ -0,0 +1,11 @@
+<?php
+
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$password=$_REQUEST['pass'];
+
+
+echo "welcome ". $name . "with email " . $email . " and password " . $password ;
+
+
+?>
